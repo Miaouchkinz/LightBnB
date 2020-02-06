c632cfba8166d0d1707e2144494d294eb6d11737
Miaouchkinz <Miaouchkinz@users.noreply.github.com>
2020-02-06 12:22:59 -0500

    update function to take various search queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -93,10 +93,47 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
+
+  const queryParams = [];
+
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryString += queryParams.length ? 'AND ' : 'WHERE '
+    queryParams.push(`${options.owner_id}`)
+    queryString += `owner_id = $${queryParams.length}`
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryString += queryParams.length ? 'AND ' : 'WHERE '
+    queryParams.push(`${options.minimum_price_per_night}`)
+    queryParams.push(`${options.maximum_price_per_night}`)
+    queryString += `cost_per_night >= $${queryParams.length-1} AND cost_per_night <= $${queryParams.length}`
+  }
+
+  if (options.minimum_rating) {
+    queryString += queryParams.length ? 'AND ' : 'WHERE '
+    queryParams.push(`${options.minimum_rating}`)
+    queryString += `rating >= $${queryParams.length}`
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  return pool.query(queryString, queryParams)
   .then(res => res.rows);
 }
 
